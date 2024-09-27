725590fee3722fc932a155c0642867cb17e3b900
demianlds <demian.igi@gmail.com>
2024-09-27 13:37:19 -0300

    Hotfixes

--- script.js
+++ script.js
@@ -43,5 +43,14 @@ window.addEventListener('popstate', function(event) {
     }
 });
 
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbarCollapse.classList.remove('show');
+    });
+});
+
 
 
